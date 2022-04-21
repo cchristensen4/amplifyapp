33c9ea2a1df2b35841a07e38a067c5269d30f8eb
Chase <chase.s.christensen@gmail.com>
2022-04-21 17:20:55 +0000

    changes for symbol

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://1000logos.net/wp-content/uploads/2021/10/Batman-Logo.png" className="App-logo" alt="logo" />
         <h1>Hello from V2 This is the site by Chase</h1>
       </header>
     </div>
